dfadfea610149ff1926758e87e30625e8999758a
yumengfei <mengfei_yu@loongjoy.com>
2023-04-24 16:44:50 +0800

    spring事务

--- src/main/java/cn/yumengfei/cases/service/impl/DeptLogServiceImpl.java
+++ src/main/java/cn/yumengfei/cases/service/impl/DeptLogServiceImpl.java
@@ -5,6 +5,7 @@ import cn.yumengfei.cases.pojo.DeptLog;
 import cn.yumengfei.cases.service.DeptLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,7 +15,8 @@ public class DeptLogServiceImpl implements DeptLogService {
     private DeptLogMapper deptLogMapper;
 
     @Override
-    @Transactional //事务传播行为：有事务就加入、没有事务就新建事务
+//    @Transactional //事务传播行为：有事务就加入、没有事务就新建事务
+    @Transactional(propagation = Propagation.REQUIRES_NEW) //事务传播行为：不管是否有事务都开启新事务
     public void insert(DeptLog deptLog) {
         deptLogMapper.insert(deptLog);
     }
